8319b28147e54e27c01aee4a14f007892ac8b6b3
kosty <round.mc@yandex.by>
2023-11-23 13:53:20 -0500

    **

--- src/ATM.java
+++ src/ATM.java
@@ -17,14 +17,15 @@ public class ATM {
         // Функция для снятия денег
         public boolean Snyatie (int SumToVlojit) {
             int ostavshiesyaKupyri = SumToVlojit;
-            int hundredsToVlojit= Math.min(ostavshiesyaKupyri / 100, hundreds);
-            ostavshiesyaKupyri -= hundredsToVlojit * 100;
+            int twentiesToVlojit = Math.min(ostavshiesyaKupyri / 20, twenties);
+            ostavshiesyaKupyri -= twentiesToVlojit * 20;
 
             int fiftiesToVlojit = Math.min(ostavshiesyaKupyri / 50, fifties);
             ostavshiesyaKupyri -= fiftiesToVlojit * 50;
 
-            int twentiesToVlojit = Math.min(ostavshiesyaKupyri / 20, twenties);
-            ostavshiesyaKupyri -= twentiesToVlojit * 20;
+            int hundredsToVlojit= Math.min(ostavshiesyaKupyri / 100, hundreds);
+            ostavshiesyaKupyri -= hundredsToVlojit * 100;
+
 
             if (ostavshiesyaKupyri == 0) {
                 System.out.println("Снято купюр номиналом 100: " + hundredsToVlojit);
